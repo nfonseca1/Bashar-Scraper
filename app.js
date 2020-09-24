1ef8de09222654c0fd5a162f39f58c4ac8bc9335
Nathan <nathan.fonseca001@gmail.com>
2020-09-23 21:20:21 -0400

    Implemented worker queue with Redis and Bull. Client polling change. Config for heroku

--- app.js
+++ app.js
@@ -12,10 +12,35 @@ const fetch = require("isomorphic-fetch");
 const jsdom = require("jsdom");
 const { JSDOM } = jsdom;
 const session = require("express-session");
-const MemoryStore = require("memorystore")(session);
+const redis = require("redis");
+const redisClient = redis.createClient(process.env.REDIS_URL 
+										|| `redis://localhost:6379`);
+const RedisStore = require("connect-redis")(session);
 app.use(session({
-	secret: "It's a secret!"
+	secret: "It's a secret!",
+	store: new RedisStore({client: redisClient}),
+	saveUninitialized: false,
+	resave: false
 }))
+
+const BullQueue = require("bull");
+let scrapeQueue = new BullQueue("Scraping", process.env.REDIS_URL 
+								|| `redis://localhost:6379`);
+scrapeQueue.process(async (job) => {
+	return await processQueue(job.data);
+});
+
+scrapeQueue.on("completed", (job, result) => {
+	console.log("Job completed!");
+	let sessionKey = 'sess:' + job.data.sessionId;
+	redisClient.get(sessionKey, (err, val) => {
+		let jsonSession = JSON.parse(val);
+		jsonSession.results = result;
+		jsonSession.searchFinished = true;
+		redisClient.set(sessionKey, JSON.stringify(jsonSession));
+		console.log("SAVING: " + Date.now());
+	});
+})
 
 app.get("/", (req, res) => {
 	res.render("index.html");
@@ -23,42 +48,49 @@ app.get("/", (req, res) => {
 
 app.post("/", async (req, res) => {
 	req.session.searchFinished = false;
-	req.session.results = [];
+	req.session.results = null;
 	let searchTerm = req.body.keyword.toLowerCase();
-	let links = await getEventLinks();
-	
-	let results = req.session.results;
-	// Wait for all link fetch promises to resolve
-	Promise.all(links.map(getLinkContent))
-	.then((contents) => {
-		contents.forEach((pageContent, i) => {
-			let indexes = getKeywordIndexes(pageContent, searchTerm);
-			if (indexes.length > 1) {
-				let snippets = indexes.map(index => getKeywordSnippet(pageContent, searchTerm, index));
-				results.push({link: links[i], snippets});
-				req.session.save();
-			}
-		})
-		req.session.searchFinished = true;
-		req.session.save();
-	})
+
+	await scrapeQueue.add({searchTerm, sessionId: req.sessionID});
 	res.sendStatus(200);
 })
 
 app.get("/status", (req, res) => {
-	let obj = {
-		results: req.session.results,
-		searchFinished: req.session.searchFinished
-	}
-	res.send(obj);
+	console.log("RELOADING: " + Date.now());
+	req.session.reload(() => {
+		let obj = {
+			results: req.session.results,
+			searchFinished: req.session.searchFinished
+		}
+		res.send(obj);
+	})
 })
 
-app.listen(process.env.PORT || 3000, process.env.IP, () => {
+app.listen(process.env.PORT || 3000, () => {
 	console.log("server started");
 })
 
+
+async function processQueue(data) {
+	let links = await getEventLinks();
+	
+	let results = [];
+	// Wait for all link fetch promises to resolve
+	await Promise.all(links.map(getLinkContent))
+	.then((contents) => {
+		contents.forEach((pageContent, i) => {
+			let indexes = getKeywordIndexes(pageContent, data.searchTerm);
+			if (indexes.length > 1) {
+				let snippets = indexes.map(index => getKeywordSnippet(pageContent, data.searchTerm, index));
+				results.push({link: links[i], snippets});
+			}
+		})
+	})
+	return results;
+}
+
 let getEventLinks = () => new Promise(async (resolve) => {
-	const response = await fetch("https://basharstore.com/2015");
+	const response = await fetch("https://basharstore.com/");
 	const text = await response.text();
 	const dom = await new JSDOM(text);
 
@@ -71,7 +103,9 @@ let getLinkContent = (link) => new Promise(async (resolve) => {
 	let text = await response.text();
 	let dom = await new JSDOM(text);
 
-	resolve(dom.window.document.querySelector(".main").textContent);
+	let main = dom.window.document.querySelector(".main")?.textContent;
+	if (main === undefined) console.error("Main content couldn't be retrieved for: " + link);
+	resolve(main);
 })
 
 let getKeywordIndexes = (content, keyword)  => {
